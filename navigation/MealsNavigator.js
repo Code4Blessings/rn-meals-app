4a5b12dc1d253ee0332315b578499d6821db7c2c
Code4Blessings <robincw35@gmail.com>
2021-05-09 16:17:44 -0400

    Fixed react-navigation-stack pkg

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,2 +1,15 @@
-import { createStatck } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailsScreen from '../screens/MealDetailsScreen';
+
+const MealsNavigator = createStackNavigator({
+    Categories: CategoriesScreen,
+    CategoryMeals: CategoryMealsScreen,
+    MealDetail: MealDetailsScreen,
+
+});
+
+export default createAppContainer(MealsNavigator);

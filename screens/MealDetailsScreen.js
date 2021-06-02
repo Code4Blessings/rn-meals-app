294642734253ef204ef0f2e9692ebdae8effad60
Code4Blessings <robincw35@gmail.com>
2021-06-02 14:40:13 -0400

    Updated MealDetailsScreen

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button, Image, ScrollView } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const MealDetailsScreen = props => {
     const mealId = props.navigation.getParam('mealId');
@@ -9,14 +12,25 @@ const MealDetailsScreen = props => {
      const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+            <View style={styles.details}>
+                <DefaultText>{selectedMeal.duration}m</DefaultText>
+                <DefaultText>{selectedMeal.complexity}</DefaultText>
+                <DefaultText>{selectedMeal.affordability}</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => <Text key={ingredient}>{ingredient}</Text>)}
+            <Text style={styles.title}>Steps</Text>
+            {selectedMeal.steps.map(step => <Text key={step}>{step}</Text>)}
+        </ScrollView>
     );
 };
 
 const styles = StyleSheet.create({
-    screen: {flex: 1, justifyContent: 'center', alignItems: 'center'}
+    image: {width: '100%', height: 200},
+    details: { flexDirection: 'row', padding: 15, justifyContent: "space-around"},
+    title: {fontFamily: 'open-sans-bold', fontSize: 22, textAlign: 'center'}
 });
 
 export default MealDetailsScreen;
